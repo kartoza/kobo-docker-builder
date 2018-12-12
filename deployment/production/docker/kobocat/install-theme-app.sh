9c0c799d9b5249078d489adecccb8127d304a61d
Boney Bun <boney.bun@gmail.com>
2018-12-12 09:50:06 +0700

    update kobocat production image to using private key

--- deployment/production/docker/kobocat/install-theme-app.sh
+++ deployment/production/docker/kobocat/install-theme-app.sh
@@ -2,9 +2,16 @@
 
 echo "Checking Custom theme app settings"
 
-if [ ! -z "CUSTOM_KOBOCAT_THEME_APP_PIP_URL" ]; then
+if [ ! -z "$CUSTOM_KOBOCAT_THEME_APP_PIP_URL" ]; then
 
-	echo "Install custom theme: CUSTOM_KOBOCAT_THEME_APP_PIP_URL"
+	echo "Install custom theme: $CUSTOM_KOBOCAT_THEME_APP_PIP_URL"
+	if [ ! -z "$CUSTOM_THEME_DEPLOY_KEY" ]; then
+	    mkdir -p /root/.ssh
+        cp $CUSTOM_THEME_DEPLOY_KEY  /root/.ssh/id_rsa
+        chmod -R 600 /root/.ssh/id_rsa
+        touch /root/.ssh/known_hosts
+        ssh-keyscan github.com >> /root/.ssh/known_hosts
+    fi
 
-	git clone --branch ${KOBOCAT_TEMPLATE_TAG} --depth 1 ${CUSTOM_KOBOCAT_THEME_APP_PIP_URL} custom_kobocat_theme
+	pip install --upgrade $CUSTOM_KOBOCAT_THEME_APP_PIP_URL
 fi
