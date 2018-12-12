a4f1e85fec1ad4a7d63a7b6092086e20d1989ef5
Boney Bun <boney.bun@gmail.com>
2018-12-12 12:00:53 +0700

    modify install-theme-app

--- deployment/production/docker/kobocat/install-theme-app.sh
+++ deployment/production/docker/kobocat/install-theme-app.sh
@@ -7,7 +7,7 @@ if [ ! -z "$CUSTOM_KOBOCAT_THEME_APP_PIP_URL" ]; then
 	echo "Install custom theme: $CUSTOM_KOBOCAT_THEME_APP_PIP_URL"
 	if [ ! -z "$CUSTOM_KOBOCAT_THEME_DEPLOY_KEY" ]; then
 	    mkdir -p /root/.ssh
-        cp CUSTOM_KOBOCAT_THEME_DEPLOY_KEY  /root/.ssh/id_rsa
+        echo "$CUSTOM_KOBOCAT_THEME_DEPLOY_KEY" > /root/.ssh/id_rsa
         chmod -R 600 /root/.ssh/id_rsa
         touch /root/.ssh/known_hosts
         ssh-keyscan github.com >> /root/.ssh/known_hosts
