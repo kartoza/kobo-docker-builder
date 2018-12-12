508e0250bbba8d113d89a8360fad2bb86ae4bb57
Boney Bun <boney.bun@gmail.com>
2018-12-12 09:57:33 +0700

    change variable name for kobocat deploy key

--- deployment/production/docker/kobocat/install-theme-app.sh
+++ deployment/production/docker/kobocat/install-theme-app.sh
@@ -5,9 +5,9 @@ echo "Checking Custom theme app settings"
 if [ ! -z "$CUSTOM_KOBOCAT_THEME_APP_PIP_URL" ]; then
 
 	echo "Install custom theme: $CUSTOM_KOBOCAT_THEME_APP_PIP_URL"
-	if [ ! -z "$CUSTOM_THEME_DEPLOY_KEY" ]; then
+	if [ ! -z "$CUSTOM_KOBOCAT_THEME_DEPLOY_KEY" ]; then
 	    mkdir -p /root/.ssh
-        cp $CUSTOM_THEME_DEPLOY_KEY  /root/.ssh/id_rsa
+        cp CUSTOM_KOBOCAT_THEME_DEPLOY_KEY  /root/.ssh/id_rsa
         chmod -R 600 /root/.ssh/id_rsa
         touch /root/.ssh/known_hosts
         ssh-keyscan github.com >> /root/.ssh/known_hosts
