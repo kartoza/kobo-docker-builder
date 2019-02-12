46de7d3800f1d1e5ca300264162de3eb2a9097f1
Rizky Maulana Nugraha <lana.pcfre@gmail.com>
2019-02-13 01:33:34 +0700

    Move modified backup scripts

--- deployment/scripts/mongo/backup_mongo.bash
+++ deployment/scripts/mongo/backup_mongo.bash
@@ -2,7 +2,12 @@
 set -e
 
 # We are using other backup deduplication so we don't need timestamp on this.
-backup_filename="mongo_backup.mongorestore.tar.gz"
+if [ -z "${MONGO_BACKUP_ARCHIVE_FILENAME:-}" ]; then
+	timestamp="$(date +%Y.%m.%d.%H_%M)"
+	backup_filename="mongo_backup__${timestamp}.mongorestore.tar.gz"
+else
+	backup_filename="${MONGO_BACKUP_ARCHIVE_FILENAME}"
+fi
 
 cd /srv/backups
 rm -rf "/srv/backups/${backup_filename}" || true
