edf3bad32898db31f9bcaa01907ef5e56a1adf22
Rizky Maulana Nugraha <lana.pcfre@gmail.com>
2019-02-13 01:33:34 +0700

    Move modified backup scripts

--- deployment/scripts/postgres/backup_postgres.bash
+++ deployment/scripts/postgres/backup_postgres.bash
@@ -2,6 +2,12 @@
 set -e
 
 # We are using other backup deduplication so we don't need timestamp on this.
+if [ -z "${POSTGRES_BACKUP_ARCHIVE_FILENAME:-}" ]; then
+	timestamp="$(date +%Y.%m.%d.%H_%M)"
+	backup_filename="postgres_backup_${KOBO_POSTGRES_DB_NAME}__${timestamp}.pg_restore"
+else
+	backup_filename="${POSTGRES_BACKUP_ARCHIVE_FILENAME}"
+fi
 backup_filename="postgres_backup_${KOBO_POSTGRES_DB_NAME}.pg_restore"
 rm -rf "/srv/backups/${backup_filename}" || true
 
